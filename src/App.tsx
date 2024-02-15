8b5020af80b2fc1c7bf5d99ec5117bd50dc4ac12
b4ngs <160027128+b4ngs@users.noreply.github.com>
2024-02-15 09:16:11 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,22 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
+      "Ay,No?",
+      "Tulungan kita? Sa mga kailangan ng tulong?",
+      "Sunduin kita?",
+      "Hatid din kita pauwi",
       "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
-      ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
-      "No :(",
+      "Samgyupsal and soju?",
+      "G boss?",
+      "Sads :|",
+      "Final answer?",
+      "Super final?",
+      "Final na final na?",
+      "Sure?",
+      "Sure na sure?",
+      "Sure na sure na sure?",
+      ":|",
+      "Bounce. :|",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -38,7 +38,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">Yes, salamat boss ;))</div>
         </>
       ) : (
         <>
@@ -46,7 +46,7 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">Uuwi ka sa friday?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
